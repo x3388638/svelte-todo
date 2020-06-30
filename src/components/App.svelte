c9714bfd91956782ea0b75c18c2041cb87e65194
YY <z3388638@gmail.com>
2020-07-01 00:09:38 +0800

    Set dev server

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -4,4 +4,4 @@
   }
 </style>
 
-<div>hello world</div>
+<div>hello world~~~</div>
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  mode: process.env.NODE_ENV,
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -32,5 +32,12 @@ module.exports = {
     new CopyPlugin({
       patterns: [{ from: 'public', to: '' }]
     })
-  ]
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    host: '0.0.0.0',
+    hot: true
+  }
 }
